ed2ed3067388e79b4e2040d37a7adf6f0f414c30
jackdunne2345 <jack.dunne3@outlook.com>
2024-10-14 19:40:37 +0100

    player movment

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-// pub mod raycaster {
-//     pub mod view {
-//         pub mod image;
-//         pub mod render;
-//     }
-//     pub mod model {
-//         pub mod vector;
-//     }
-//     pub mod controller {
-//         pub mod ray;
-//         pub mod sphere;
-//     }
-// }
-
-// use raycaster::model::vector::Vec3;
-// use raycaster::view::image::save_image;
-// use raycaster::view::render::render;
-// fn main() {
-//     let width = 800;
-//     let height = 600;
-//     let buffer: Vec<Vec<Vec3>> = render(width, height);
-//     save_image(width, height, buffer);
-// }
 use raylib::prelude::*;
 
 const MAP_WIDTH: usize  = 24;
@@ -103,6 +80,7 @@ const WORLD_MAP: [[i32; MAP_WIDTH]; MAP_HEIGHT] = [
     ],
 ];
 
+
 fn main() {
     let mut pos_x = 22.0;
     let mut pos_y = 12.0;
@@ -111,7 +89,7 @@ fn main() {
     let mut plane_x = 0.0;
     let mut plane_y = 0.66;
     let mut time = 0.0;
-    let mut olde_time = 0.0;
+    let mut old_time = 0.0;
     let (mut rl, thread) = raylib::init().size(SCREEN_WIDTH, SCREEN_HEIGHT).title("RayCaster").build();
     while !rl.window_should_close() {
                 let mut d = rl.begin_drawing(&thread);
@@ -131,7 +109,7 @@ fn main() {
                     //length of ray from one x or y side to next x or y side
                     let delta_dist_x :f64= if ray_dir_x == 0.0 { 1e30} else { (1.0/ray_dir_x).abs() };
                     let delta_dist_y :f64= if ray_dir_y == 0.0 { 1e30} else { (1.0/ray_dir_y).abs() };
-                    let mut perp_wall_dist:f64;
+                    let perp_wall_dist:f64;
                     // Direction to step in x or y-direction (either +1 or -1)
                     let step_x: i32; // Declare variable for X direction
                     let step_y: i32; // Declare variable for Y direction
@@ -171,24 +149,56 @@ fn main() {
                         else{ perp_wall_dist=side_dist_y-delta_dist_y}
                     let line_height: i32=(SCREEN_HEIGHT as f64/perp_wall_dist) as i32;
                     let mut draw_start=-line_height/2+SCREEN_HEIGHT/2;
-                    if(draw_start <0){draw_start=0}
+                    if draw_start <0 {draw_start=0}
                     let mut draw_end=line_height/2+SCREEN_HEIGHT/2;
-                    if(draw_end>=SCREEN_HEIGHT){draw_end=SCREEN_HEIGHT-1}
+                    if draw_end>=SCREEN_HEIGHT {draw_end=SCREEN_HEIGHT-1}
                     let mut color_rgb: Color = match WORLD_MAP[map_x as usize][map_y as usize] {
                         1 => Color::RED,    
                         2 => Color::GREEN,   
                         3 => Color::BLUE,   
                         4 => Color::VIOLET,   
+                        5 => Color::YELLOWGREEN,   
                         _ => Color::WHITE,   
                     };
-                    if(side ==1){color_rgb=color_rgb.brightness(0.5)}
+                    if side ==1 {color_rgb=color_rgb.brightness(0.5)}
                     //draws the line using raylib
                     d.draw_line(x, draw_start, x,draw_end, color_rgb);
                     
+                    old_time=time;
+                    time=d.get_time();
+                    let frame_time:f64=((time -old_time)/1000.0) as f64;
+                    // print!("{}",1.0/frame_time);
+                 
+                    let move_speed=frame_time*10000.0;
+                    let rot_speed=frame_time*3000.0;
+
+                 
+                    if d.is_key_down(KeyboardKey::KEY_W ){
+                        if WORLD_MAP[(pos_x+dir_x*move_speed)as usize][pos_y as usize]==0 {pos_x+=dir_x*move_speed}
+                        if WORLD_MAP[pos_x as usize][(pos_y+dir_y*move_speed)as usize]==0 {pos_y+=dir_y*move_speed}
+                    }
+                    if d.is_key_down(KeyboardKey::KEY_S ){
+                        if WORLD_MAP[(pos_x-dir_x*move_speed)as usize][pos_y as usize]==0 {pos_x-=dir_x*move_speed}
+                        if WORLD_MAP[pos_x as usize][(pos_y-dir_y*move_speed)as usize]==0 {pos_y-=dir_y*move_speed}
+                    }
+                    if d.is_key_down(KeyboardKey::KEY_A){
+                        let old_dir_x=dir_x;
+                        dir_x=dir_x* rot_speed.cos()-dir_y*rot_speed.sin();
+                        dir_y=old_dir_x*rot_speed.sin()+dir_y* rot_speed.cos();
+                        let old_plane_x=plane_x;
+                        plane_x=plane_x* rot_speed.cos()-plane_y*rot_speed.sin();
+                        plane_y=old_plane_x*rot_speed.sin()+plane_y* rot_speed.cos();
+                    }
+                    if d.is_key_down(KeyboardKey::KEY_D ){
+                        let old_dir_x=dir_x;
+                        dir_x=dir_x* (-rot_speed).cos()-dir_y*(-rot_speed).sin();
+                        dir_y=old_dir_x*(-rot_speed).sin()+dir_y* (-rot_speed).cos();
+                        let old_plane_x=plane_x;
+                        plane_x=plane_x* (-rot_speed).cos()-plane_y*(-rot_speed).sin();
+                        plane_y=old_plane_x*(-rot_speed).sin()+plane_y* (-rot_speed).cos();
+                    }
                     
-                    
-                    
-                }
+                 d.clear_background(Color::BLACK);
                
             }
-}
+}}
